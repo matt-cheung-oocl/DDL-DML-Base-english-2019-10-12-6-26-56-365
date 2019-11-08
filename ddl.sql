d180639691aa1f426c8ede0a393da4f0a6987ff7
Matt Cheung <matt.cheung@oocl.com>
2019-11-08 16:52:42 +0800

    done

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+SHOW databases;
 -- Enter a certain database
-
+USE database_name;
 -- Create a database
-
+CREATE DATABASE database_name;
 -- Create the database of the designated character set
-
+CREATE DATABASE database_name character set utf8;
 -- Display the creation information fo the database
-
+SHOW  CREATE DATABASE database_name;
 -- Revise the codes of the database
-
+ALTER DATABASE database_name character set gb2312;
 -- Delete a database
-
+DROP DATABASE database_name;
 -- **Table level**
 -- Revise table name
-
+ALTER TABLE student RENAME Students;   
 -- Revise the field's data type
-
+ALTER TABLE Students MODIFY name varchar(10);
 -- Revise field name
-
+ALTER TABLE Students CHANGE sex gender varchar(10); 
 -- Add field
-
+ALTER TABLE Students ADD height int;   
 -- Delete field
-
+ALTER TABLE Students DROP age;        
 -- Revise the table's storage engine
-
+ALTER TABLE Students ENGINE=MyISAM;
 -- Delete the table's foreign key restriant
-
+ALTER TABLE Students DROP FOREIGN KEY id;
 -- Delete a table
+DROP TABLE Students;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,15 @@
 -- Insert record
+-- INSERT INTO tablename(field1,field2...fieldn) VALUES(value1,value2...valuen);
+INSERT INTO student(id, name, age, sex) VALUES('003', 'Peter', '20', 'male');
 
 -- Revise record
+-- UPDATE tablename SET field1=value1 [WHERE CONDITION];
+UPDATE student SET age=19 WHERE name='Peter';
 
---  Delete record
+-- Delete record
+-- DELETE FROM tablename [WHERE CONDITION];
+DELETE FROM student WHERE name='Emma';
 
 -- Search record
+-- SELECT * FROM tablename[WHERE CONDITION];
+SELECT * FROM student WHERE id='001';
